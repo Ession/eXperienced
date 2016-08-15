9567f2e309b9b3be9927893b57ebc141eec0230f
Ession <mail@mathiasjost.com>
2016-08-15 09:11:21 +0200

    * Initial xp bar without color or styling.
    Signed-off-by: Ession <mail@mathiasjost.com>

--- eXperienced.lua
+++ eXperienced.lua
@@ -10,6 +10,21 @@
 --
 -- =============================================================================
 
+-- -----------------------------------------------------------------------------
+-- Return rested XP percentage
+-- -----------------------------------------------------------------------------
+local function XPRestedPercent()
+	return GetXPExhaustion("player") * 100 / UnitXPMax("player")
+end
+
+
+-- -----------------------------------------------------------------------------
+-- Return current XP percentage
+-- -----------------------------------------------------------------------------
+local function XPCurrentPercent()
+	return UnitXP("player") * 100 / UnitXPMax("player")
+end
+
 
 -- -----------------------------------------------------------------------------
 -- create the addon frame
@@ -29,6 +44,8 @@ eXperienced:Show()
 local XPCurrent
 local XPMax
 local XPRested
+local XPRestedBarWidth
+local XPCurrentBarWidth
 
 
 -- -----------------------------------------------------------------------------
@@ -47,14 +64,11 @@ eXperienced:SetScript("OnEvent", function(self, event, ...)
 	XPCurrent = UnitXP("player")
 	XPMax = UnitXPMax("player")
 	XPRested = GetXPExhaustion("player")
-	XPCurrentPercent = XPCurrent * 100 / XPMax
-	XPRestedPercent = XPRested * 100 / XPMax
 
 	-- set the text display to the current values
-	ExperienceText:SetText(string.format("%d/%d (%.1f%%) R: %.1f%%", XPCurrent, XPMax, XPCurrentPercent, XPRestedPercent))
+	ExperienceText:SetText(string.format("%d/%d (%.1f%%) R: %.1f%%", XPCurrent, XPMax, XPCurrentPercent(), XPRestedPercent()))
 
 end)
-
 
 
 -- -----------------------------------------------------------------------------
@@ -65,6 +79,46 @@ ExperienceText:SetPoint("CENTER", 0, -10)
 ExperienceText:SetFont("Interface\\AddOns\\eStats\\font.ttf", 14)
 ExperienceText:SetTextColor(1, 1, 1)
 ExperienceText:Hide()
+
+
+-- -----------------------------------------------------------------------------
+-- create the rested xp bar display
+-- -----------------------------------------------------------------------------
+if XPRestedPercent() + XPCurrentPercent() >= 100 then
+	XPRestedBarWidth = GetScreenWidth()
+else
+	XPRestedBarWidth = (XPRestedPercent() + XPCurrentPercent()) * GetScreenWidth() / 100
+end
+
+local eXperiencedRested = CreateFrame("Frame", "eXperiencedRested", eXperienced)
+
+eXperiencedRested:SetFrameStrata("BACKGROUND")
+eXperiencedRested:SetWidth(XPRestedBarWidth)
+eXperiencedRested:SetHeight(1)  
+eXperiencedRested:SetAlpha(1.0)
+eXperiencedRested:SetBackdrop({bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background"})
+eXperiencedRested:SetBackdropColor(1,1,1,1)
+
+eXperiencedRested:SetPoint("TOPLEFT")
+eXperiencedRested:Show()
+
+
+-- -----------------------------------------------------------------------------
+-- create the current xp bar display
+-- -----------------------------------------------------------------------------
+XPCurrentBarWidth = XPCurrentPercent() * GetScreenWidth() / 100
+
+local eXperiencedCurrent = CreateFrame("Frame", "eXperiencedCurrent", eXperienced)
+
+eXperiencedCurrent:SetFrameStrata("BACKGROUND")
+eXperiencedCurrent:SetWidth(XPCurrentBarWidth)
+eXperiencedCurrent:SetHeight(1)  
+eXperiencedCurrent:SetAlpha(1.0)
+eXperiencedCurrent:SetBackdrop({bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background"})
+eXperiencedCurrent:SetBackdropColor(0,0,1,1)
+
+eXperiencedCurrent:SetPoint("TOPLEFT")
+eXperiencedCurrent:Show()
 
 
 -- -----------------------------------------------------------------------------
